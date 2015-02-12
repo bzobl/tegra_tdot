70deeb30366b5eff3de63e51fb155b0bc43f0717
Bernd Zobl <bernd.zobl@students.fh-hagenberg.at>
2015-02-12 21:04:24 +0100

    fix

--- main.cpp
+++ main.cpp
@@ -14,10 +14,10 @@ using namespace cv::gpu;
 using timepoint = std::chrono::time_point<std::chrono::high_resolution_clock>;
 
 template <typename T>
-inline T mapVal(T x, T a, T b, T c, T d)
+inline T mapValue(T x, T a, T b, T c, T d)
 {
-    x = ::max(::min(x, b), a);
-    return c + (d-c) * (x-a) / (b-a);
+    x = clamp(x, a, b);
+    return c + (d - c) * (x - a) / (b - a);
 }
 
 static void colorizeFlow(const Mat &u, const Mat &v, Mat &dst)
@@ -36,8 +36,8 @@ static void colorizeFlow(const Mat &u, const Mat &v, Mat &dst)
         for (int x = 0; x < u.cols; ++x)
         {
             dst.at<uchar>(y,3*x) = 0;
-            dst.at<uchar>(y,3*x+1) = (uchar)mapVal(-v.at<float>(y,x), -dMax, dMax, 0.f, 255.f);
-            dst.at<uchar>(y,3*x+2) = (uchar)mapVal(u.at<float>(y,x), -dMax, dMax, 0.f, 255.f);
+            dst.at<uchar>(y,3*x+1) = (uchar)mapValue(-v.at<float>(y,x), -dMax, dMax, 0.f, 255.f);
+            dst.at<uchar>(y,3*x+2) = (uchar)mapValue(u.at<float>(y,x), -dMax, dMax, 0.f, 255.f);
         }
     }
 }
@@ -61,6 +61,44 @@ cv::Mat &&use_farneback(cv::gpu::GpuMat *last, cv::gpu::GpuMat *now,
   download_stop = std::chrono::high_resolution_clock::now();
 
   colorizeFlow(flowx, flowy, result);
+}
+
+void getFlowField(const Mat& u, const Mat& v, Mat& flowField)
+{
+    float maxDisplacement = 1.0f;
+
+    for (int i = 0; i < u.rows; ++i)
+    {
+        const float* ptr_u = u.ptr<float>(i);
+        const float* ptr_v = v.ptr<float>(i);
+
+        for (int j = 0; j < u.cols; ++j)
+        {
+            float d = max(fabsf(ptr_u[j]), fabsf(ptr_v[j]));
+
+            if (d > maxDisplacement)
+                maxDisplacement = d;
+        }
+    }
+
+    flowField.create(u.size(), CV_8UC4);
+
+    for (int i = 0; i < flowField.rows; ++i)
+    {
+        const float* ptr_u = u.ptr<float>(i);
+        const float* ptr_v = v.ptr<float>(i);
+
+
+        Vec4b* row = flowField.ptr<Vec4b>(i);
+
+        for (int j = 0; j < flowField.cols; ++j)
+        {
+            row[j][0] = 0;
+            row[j][1] = static_cast<unsigned char> (mapValue (-ptr_v[j], -maxDisplacement, maxDisplacement, 0.0f, 255.0f));
+            row[j][2] = static_cast<unsigned char> (mapValue ( ptr_u[j], -maxDisplacement, maxDisplacement, 0.0f, 255.0f));
+            row[j][3] = 255;
+        }
+    }
 }
 
 cv::Mat use_brox(cv::gpu::GpuMat *last, cv::gpu::GpuMat *now,
