2187d3bf7647b2b39538ff6b276033f427588d08
Bernd Zobl <bernd.zobl@students.fh-hagenberg.at>
2015-02-18 02:59:18 +0100

    debug

--- main.cpp
+++ main.cpp
@@ -109,6 +109,7 @@ void capture_loop(LiveStream &stream)
   cv::setWindowProperty(live_feed_window, CV_WND_PROP_FULLSCREEN, CV_WND_PROP_FULLSCREEN);
   cv::moveWindow(live_feed_window, 0, 0);
   */
+  cv::resizeWindow(live_feed_window, 1920, 1080);
 
   while (!exit) {
     double t = (double) getTickCount();
