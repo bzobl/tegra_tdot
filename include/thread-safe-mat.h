b2c7e93f7d32c89a43665257ad5aa3ecca90d36a
Bernd Zobl <bernd.zobl@students.fh-hagenberg.at>
2015-02-22 12:30:05 +0100

    add threadssafemat files

---
+++ include/thread-safe-mat.h
@@ -0,0 +1,23 @@
+#ifndef THREAD_SAFE_MAT_INCLUDED
+#define THREAD_SAFE_MAT_INCLUDED
+
+#include <mutex>
+#include "opencv2/core.hpp"
+
+class ThreadSafeMat {
+
+private:
+  cv::Mat mMat;
+
+  std::mutex mMutex;
+
+public:
+  ThreadSafeMat();
+  ThreadSafeMat(cv::Mat mat);
+
+  cv::Mat &&get();
+  void update(cv::Mat new_mat);
+
+};
+
+#endif
---
+++ thread-safe-mat.cpp
@@ -0,0 +1,23 @@
+#include "thread-safe-mat.h"
+
+ThreadSafeMat::ThreadSafeMat()
+{
+}
+
+ThreadSafeMat::ThreadSafeMat(cv::Mat mat)
+{
+  mat.copyTo(mMat);
+}
+
+cv::Mat &&ThreadSafeMat::get()
+{
+  std::unique_lock<std::mutex> l(mMutex);
+  cv::Mat ret;
+  mMat.copyTo(ret);
+  return ret;
+}
+void ThreadSafeMat::update(cv::Mat new_mat)
+{
+  std::unique_lock<std::mutex> l(mMutex);
+  new_mat.copyTo(mMat);
+}
